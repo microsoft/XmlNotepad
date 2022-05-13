0380ff862acc00d5a8b1c885a15f8389bb950dd7
Chris Lovett <lovettchris@hotmail.com>
2022-05-13 14:16:46 -0700

    Publish version 2.8.0.60.

--- src/Version/Version.cs
+++ src/Version/Version.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.0.59")]
-[assembly: AssemblyFileVersion("2.8.0.59")]
+[assembly: AssemblyVersion("2.8.0.60")]
+[assembly: AssemblyFileVersion("2.8.0.60")]
