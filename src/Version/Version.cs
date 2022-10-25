05b8042cff8e57c9107a2134902ffc607891a56a
Chris Lovett <lovettchris@hotmail.com>
2022-10-24 19:16:38 -0700

    Update to SgmlReader version 1.8.26 Publish version 2.8.0.64

--- src/Version/Version.cs
+++ src/Version/Version.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.0.63")]
-[assembly: AssemblyFileVersion("2.8.0.63")]
+[assembly: AssemblyVersion("2.8.0.64")]
+[assembly: AssemblyFileVersion("2.8.0.64")]
