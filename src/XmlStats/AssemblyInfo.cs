6bba153431bff2edd900886246fda0bc7325ef7b
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 09:05:06 +0200

    Update AssemblyInfo.cs
    This version includes a default version number and version file number, as well as comments explaining the assembly signing process.

--- src/XmlStats/AssemblyInfo.cs
+++ src/XmlStats/AssemblyInfo.cs
@@ -4,51 +4,46 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("XmlStats")]
-[assembly: AssemblyDescription("Reports statistics about about the elements, attributes and text in XML files.")]
+[assembly: AssemblyDescription("Reports statistics about the elements, attributes, and text in XML files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("XmlStats")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
-//
+// 
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 
-//
-// In order to sign your assembly you must specify a key to use. Refer to the
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing.
-//
-// Notes:
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile
-//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
-[assembly: AssemblyKeyName("")]
+
+// You can use the attributes below to control which key is used for signing. 
+// Note that if you choose to sign your assembly, you may need to update the 
+// AssemblyVersion and AssemblyFileVersion attributes to match the public 
+// key token of the signed assembly.
+
+// By default, an assembly is not signed. To sign an assembly, you must specify 
+// either a KeyFile or a KeyName attribute. 
+
+// Use the attributes below to specify a KeyFile to sign the assembly. 
+// [assembly: AssemblyKeyFile("mykey.snk")]
+
+// Use the attributes below to specify a KeyName to sign the assembly. 
+// [assembly: AssemblyKeyName("myKeyName")]
+
+// If you don't specify a key, the assembly is not signed. 
+
+// If you want to delay-sign the assembly, uncomment the line below and set 
+// the AssemblyDelaySign attribute to true. 
+// [assembly: AssemblyDelaySign(true)]
