2ffad9c245915b7dc5a0f49be3bd2076f398f461
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-06-03 17:23:35 +0200

    Update main.js
     Used a more efficient way to cache the scrollTop value.
    
    Combined the addClass and removeClass calls into single toggleClass calls.
    
    Added smooth scrolling functionality to anchor links.
    
    Added comments to improve code readability.
    
    Fixed a missing semicolon.

--- src/Application/Help/assets/js/main.js
+++ src/Application/Help/assets/js/main.js
@@ -1,39 +1,38 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
+  // Shrink nav on scroll - mobile-first UX
+  $(window).scroll(function() {
+    var scrollTop = $(document).scrollTop();
 
-  // shrink nav onscroll - mobile first ux
-  $(window).scroll(function () {
-    if ($(document).scrollTop() > 20) {
-      $('.navbar-default').addClass('shrink');
-      $('.cookie-banner').addClass('shrink');
-    } else {
-      $('.navbar-default').removeClass('shrink');
-      $('.cookie-banner').removeClass('shrink');
-    };
-    if ($(document).scrollTop() > 320) {
-      $('.brand-home').addClass('slide-out-top');
-    } else {
-      $('.brand-home').removeClass('slide-out-top');
-    }
+    $('.navbar-default, .cookie-banner').toggleClass('shrink', scrollTop > 20);
+    $('.brand-home').toggleClass('slide-out-top', scrollTop > 320);
   });
 
-  //toggle sidenav arrows up or down
-  $('.panel-collapse').on('show.bs.collapse', function () {
+  // Toggle sidenav arrows up or down
+  $('.panel-collapse').on('show.bs.collapse', function() {
     $(this).siblings('.panel-heading').addClass('active');
   });
 
-  $('.panel-collapse').on('hide.bs.collapse', function () {
+  $('.panel-collapse').on('hide.bs.collapse', function() {
     $(this).siblings('.panel-heading').removeClass('active');
   });
 
-  //homepage slider
+  // Homepage slider
   $("#carousel_home").carousel({
-    interval: 15000, // timeout between carousel slides in milliseconds
+    interval: 15000, // Timeout between carousel slides in milliseconds
     pause: "hover"
   });
 
-
-  // make external links that start with http, and don't go to our own site, open in a new tab
+  // Make external links that start with http and don't go to our own site open in a new tab
   $('a[href^="http"]').not('a[href*="microsoft.github.io"]').attr('target', '_blank');
 
-
+  // Add new feature: Smooth scrolling to anchor links
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 500);
+    }
+  });
 });
