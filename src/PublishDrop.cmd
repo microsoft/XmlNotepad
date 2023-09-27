84da0e0196b23b9acb7f94f820b39b719fe300b4
Chris Lovett <lovettchris@hotmail.com>
2023-09-26 23:55:32 -0700

    Publish version xmlnotepad_2.9.0.6

--- src/PublishDrop.cmd
+++ src/PublishDrop.cmd
@@ -11,7 +11,6 @@ if "%TargetDir%"=="" (
 
 echo SrcDir=%SrcDir%
 set "DropDir=%SrcDir%drop"
-set "HelpDir=%SrcDir%drop\Help"
 set "SamplesDir=%SrcDir%drop\samples"
 
 if exist "!DropDir!" (
@@ -22,7 +21,6 @@ if exist "!DropDir!" (
 echo Creating directories...
 mkdir "!DropDir!"
 mkdir "!DropDir!\runtimes"
-mkdir "!HelpDir!"
 mkdir "!SamplesDir!"
 
 echo TargetDir=!TargetDir!
@@ -35,11 +33,6 @@ set RawTarget=%RawTarget%
 echo Copying updates to !RawTarget! and drop directory...
 xcopy /y "%SrcDir%Updates\*" "!RawTarget!"
 xcopy /y "%SrcDir%Updates\*" "!DropDir!"
-
-echo Copying help to !HelpDir! and %RawTarget%Help...
-xcopy /s /y "%SrcDir%Application\Help" "!HelpDir!"
-if not exist "%RawTarget%\Help" mkdir "%RawTarget%Help"
-xcopy /s /y "%SrcDir%Application\Help" "%RawTarget%Help"
 
 echo Copying samples to !SamplesDir!...
 xcopy /y "%SrcDir%Application\Samples\*.*" "!SamplesDir!"
