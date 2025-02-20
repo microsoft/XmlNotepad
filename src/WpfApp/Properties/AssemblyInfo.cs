868e74a0c3dc622e1f484a83e35c6dc901988ce5
Chris Lovett <clovett@microsoft.com>
2025-02-20 13:38:36 -0800

    update copyrights.

--- src/WpfApp/Properties/AssemblyInfo.cs
+++ src/WpfApp/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
